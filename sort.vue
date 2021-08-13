2e6da6d901bd3a539099db15d18a812a0f5fd08d
Natsunokazeme <79150294+Natsunokazeme@users.noreply.github.com>
2021-08-14 06:08:10 +0800

    Add files via upload

---
+++ sort.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="sort-tittle">字母排序</div>
+        <ul class="sort-char" >
+            <li v-for="(val,key,index) in cities" :key="index " v-on:click="doSomething(key)"
+            >{{key}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props:['cities'],
+    methods:{
+        doSomething(key){
+            alert(key);
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.sort-tittle{
+    color: #212121;
+    font-size: .28rem;
+    padding: .3rem;
+}
+.sort-char{
+    background: #fff;
+    overflow: hidden;
+}
+.sort-char li{
+    float:left;
+    width: 16.6%;
+    font-size: .3rem;
+    line-height: .9rem;
+    text-align: center;
+    background: #fff;
+}
+</style>
